4b08a8a736d24cb902981332d2609d32a8573b7c
davimehra <davimehra1234@gmail.com>
2023-10-09 21:03:43 +0530

    Functions_Introduction_WithOut Arguments

---
+++ Session14_JS_Init/script.js
@@ -0,0 +1,26 @@
+console.log(new Date().toISOString(), "\n\n");
+
+console.log("Datatypes");
+let Myname = "My name is Rishaan. My Class is Eight"; // string (combination of Character)
+// let char1 = "U"; // char
+// let char2 = "O"; // char
+
+let height = 5.1; // number
+let areYouBoy = true; // boolean
+
+let state; // declare
+state = 100; // initialization =  value+type;
+
+let city; // undefined
+let component = Number(); // Providing Data type directly
+
+component = null;
+
+console.log(typeof Myname, " - ", "My Name is = ", Myname);
+console.log(typeof height, " - ", "My Height is = ", height);
+console.log(typeof areYouBoy, " - ", "I am Boy = ", areYouBoy);
+
+console.log(typeof state, " - ", "State = ", state);
+console.log(typeof city, " - ", "city = ", city);
+
+console.log(component);

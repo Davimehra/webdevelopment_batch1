3c4083b844f26a9ce8eb6da820509e0b7ebac956
davimehra <davimehra1234@gmail.com>
2023-10-25 20:36:18 +0530

    Session18 Completed - Conditional Statement & Teritary Statement

---
+++ Session18/exampleLoop.js
@@ -0,0 +1,53 @@
+// for(initial;condition;Iteration){}
+
+for (let x = 0; x < 20; x++) {
+  console.log(x, "value");
+}
+
+let s1_hindiMarks = 50;
+let s1_englishMarks = 40;
+let s1_MathsMarks = 80;
+let s1_ScienceMarks = 50;
+let s1_PunjabiMarks = 60;
+
+let s2_hindiMarks = 50;
+let s2_englishMarks = 40;
+let s2_MathsMarks = 80;
+let s2_ScienceMarks = 50;
+let s2_PunjabiMarks = 60;
+
+let s3_hindiMarks = 50;
+let s3_englishMarks = 40;
+let s3_MathsMarks = 80;
+let s3_ScienceMarks = 50;
+let s3_PunjabiMarks = 60;
+
+// SumS1
+// PercentageS1
+
+// SumS2
+// PercentageS2
+
+// SumS3
+// PercentageS3
+
+// console.log(Sum , " Total of ")
+// console.log(Percentage, " Percentage")
+
+// condition , Top Student
+// Sum , Percentage
+
+// if(s1M>s2M){
+//     if(s1M>s3M){
+//         console.log("Sumof S1 Marks, Perventage")
+//     }else{
+//         console.log("Sumof S3 Marks, Perventage")
+//     }
+
+// }else{
+//     if(s2M>s3M){
+
+//     }else{
+
+//     }
+// }

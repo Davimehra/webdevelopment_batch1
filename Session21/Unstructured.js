fcc686b0027bae5fe886cbb2bb3dc7d0320147cd
davimehra <davimehra1234@gmail.com>
2023-11-01 20:59:22 +0530

    Session21- Arrays, ForLoopingArrays, Trail FilteringArrays

---
+++ Session21/Unstructured.js
@@ -0,0 +1,15 @@
+let name1 = "Rishaan";
+let rollno1 = 1;
+
+let name2 = "Parvinder";
+let rollno2 = 2;
+
+let name3 = "Kushveer";
+let rollno3 = 3;
+
+totalStudents = 40;
+
+totalStudentRollno = 40;
+totalStudentNames = 40;
+
+totalVariables = 80;

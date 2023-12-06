e1023bda12fb1ded1b7520ef6bbf5a39f19cb137
Davimehra <davimehra1234@gmail.com>
2023-12-06 21:18:05 +0530

    Bucket Project Completed - Homework delete all child from list

--- Session29/index.js
+++ Session29/index.js
@@ -7,10 +7,38 @@ const bucketList = document.getElementById("bucketList");
 
 function onClickHandler() {
   const listItem = document.createElement("li");
+
   // <li> </li>
 
-  listItem.innerText = itemInput.value;
+  // listItem.innerText = itemInput.value;
   // <li>bats</li>
 
+  listItem.innerHTML = `<div class="liClass">
+  <span>${itemInput.value}</span>
+  <span id="trash" onclick="onClickDelete(event)">🗑️</span>  
+</div>`;
+
   bucketList.appendChild(listItem);
+}
+
+function onClickDelete(rishaanEvent) {
+  const deletingLi = rishaanEvent.target.parentElement.parentElement;
+  console.log("Delete Button Clicked");
+  bucketList.removeChild(deletingLi);
+}
+
+{
+  /* <ol>
+  <li>
+    <span>Speaker</span>
+    <span>🗑️</span>
+  </li>
+  ;
+</ol>; */
+}
+
+function onKeyDown(daviEvent) {
+  if (daviEvent.key == "Enter") {
+    onClickHandler();
+  }
 }

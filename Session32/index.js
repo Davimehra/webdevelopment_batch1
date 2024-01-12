fea31b22be09be5237bd0533b6f3a2d95e49a0cc
Davimehra <davimehra1234@gmail.com>
2024-01-12 20:30:39 +0530

    Axios created

---
+++ Session32/index.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const fs = require('fs');
+
+//https://jsonplaceholder.typicode.com/users/2
+
+// http, https (secure): protocol
+// jsonplaceholder: hostname
+// typicode.com: domain
+// users/1: route
+
+
+async function getTodo (){
+// 5 min
+ const response = await axios.get('https://jsonplaceholder.typicode.com/comments/1');
+//   console.log(Object.values(response.data))
+
+fs.writeFileSync('./data.json',JSON.stringify(response.data))
+  console.log("Saved to filed")
+  
+}
+
+getTodo()
